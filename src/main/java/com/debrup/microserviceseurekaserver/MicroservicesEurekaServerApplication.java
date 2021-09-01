701a5d6aa5ca7601e4ad26c93f294d455dbb79ea
Debrup Mondal <debrup.official.365@gmail.com>
2021-09-01 13:25:22 +0530

    eureka-server-initialized

---
+++ src/main/java/com/debrup/microserviceseurekaserver/MicroservicesEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.debrup.microserviceseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesEurekaServerApplication.class, args);
+    }
+
+}
